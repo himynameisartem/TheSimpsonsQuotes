83ee98f946800b4e0210122c5e76157ccbe6c00d
ArtemKudryavtsev <himynameisartem@icloud.com>
2025-06-09 14:24:37 +0300

    Initial Commit

---
+++ TheSimpsonsQuote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheSimpsonsQuote
+//
+//  Created by Артем Кудрявцев on 09.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
